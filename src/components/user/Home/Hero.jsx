8aeca0e4b48ff2636f752b7676463431e94abab5
mouradbalayev <muradbalazade@gmail.com>
2024-10-18 21:56:54 +0400

    homePage

--- src/components/user/Home/Hero.jsx
+++ src/components/user/Home/Hero.jsx
@@ -18,8 +18,12 @@ export default function HeroSection() {
 
     return (
         <section 
-            style={{ backgroundImage: !isDarkTheme ? `url(${heroBg})` : 'none' }}
-            className="relative sm:pt-0 pt-12 bg-gradient-to-b from-blue-100 dark:bg-gray-900 to-white min-h-screen flex items-center justify-center overflow-hidden"
+        style={{
+            backgroundImage: !isDarkTheme ? `url(${heroBg})` : 'none',
+            backgroundRepeat: 'no-repeat', // Prevent background image from repeating
+            backgroundSize: 'cover', // Make sure the background covers the section
+            backgroundPosition: 'center' // Center the background image
+        }}            className="relative sm:pt-0 pt-12 bg-gradient-to-b from-blue-100 dark:bg-gray-900 to-white min-h-[700px] md:min-h-[950px] flex items-center justify-center overflow-hidden"
         >
             {/* Background shapes */}
             <div className="absolute inset-0 z-0">
@@ -95,8 +99,8 @@ export default function HeroSection() {
                                 height={600}
                                 className="rounded-lg"
                             />
-                            <div className="absolute -bottom-6 -right-6 bg-yellow-400 dark:bg-yellow-600 rounded-full p-4 shadow-lg">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <div className="absolute -bottom-6 -right-3 bg-yellow-400 dark:bg-yellow-600 rounded-full sm:p-4 p-2 shadow-lg">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="sm:h-8 sm:w-8 w-6 h-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z" />
                                 </svg>
                             </div>
